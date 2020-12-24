1b2bf0737b24d5ceb3f53de97aafe2bb6b347c87
Matthew Mattox <mmattox@support.tools>
2020-12-24 10:32:09 -0600

    Removing debug flag

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ cd ./deploy
 for file in `ls *.yaml`
 do
   echo "Working on $file"
-  cat $file | sed "s/image:.*/image:\ cube8021\/rancher-metadata-airgap:${CI_BUILD_NUMBER}/g" > ../deploy-ready/"$file"
+  cat $file | sed "s/image:.*/image:\ cube8021\/rancher-metadata-airgap:${CI_BUILD_NUMBER}/g" | grep -v 'namespace:' > ../deploy-ready/"$file"
 done
