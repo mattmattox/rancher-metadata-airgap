1da8453b191136cfd1f99d5b4fc9451e506fcc2d
Matthew Mattox <mmattox@support.tools>
2020-12-24 09:45:33 -0600

    Setting up Drone

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "customizing Deployment files..."
+mkdir ./deploy-ready/
+cd ./deploy
+for file in `ls *.yaml`
+do
+  echo "Working on $file"
+  cat $file | sed "s/image:.*/image:\ cube8021\/rancher-metadata-airgap:${CI_BUILD_NUMBER}/g" > ../deploy-ready/"$file"
+done
